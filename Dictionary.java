20e5a211e9397de35fb05e10caad13350cc27d98
Natali <tolikalokogolik>
2022-03-11 17:08:48 +0200

    sõnastiku testimine

--- Dictionary.java
+++ Dictionary.java
@@ -5,7 +5,15 @@ public class Dictionary {
     private int difficulty = 5;
     private ArrayList<String> words = new ArrayList<>();
 
+    public Dictionary(int difficulty) {
+        this.difficulty = difficulty;
+    }
 
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    /*
     java.io.File fail = new java.io.File("dic" + difficulty +".txt");
 
     try (java.util.Scanner sc = new java.util.Scanner(fail, "UTF-8")) {
@@ -18,7 +26,24 @@ public class Dictionary {
 
         }
     }
+     */
 
+
+    public void addWord(String sona){
+        words.add(sona);
+    }
+
+    public String randomWord(){
+        return words.get((int) Math.random() * words.size());
+    }
+
+    @Override
+    public String toString() {
+        return "Dictionary{" +
+                "difficulty=" + difficulty +
+                ", words=" + words +
+                '}';
+    }
 
     public Dictionary() throws FileNotFoundException {
         System.out.println("Antud faili pole olemas");
---
+++ Test.java
@@ -0,0 +1,26 @@
+public class Test {
+    public static void main(String[] args) throws Exception{
+
+        int inputDifficulty = 5;
+
+        //loome sõnastiku ja lisame sõnad
+        Dictionary sonastik = new Dictionary(inputDifficulty);
+
+        java.io.File fail = new java.io.File( "dic" + sonastik.getDifficulty() +".txt");
+        try (java.util.Scanner sc = new java.util.Scanner(fail, "UTF-8")) {
+            while (sc.hasNextLine()) {
+                String rida = sc.nextLine();
+                String[] tükid = rida.split(" ");
+                System.out.println(tükid[2].substring(1,tükid[2].length()-1));
+                System.out.println(rida);
+                sonastik.addWord(tükid[2].substring(1,tükid[2].length()-1));
+
+
+            }
+        }
+
+        System.out.println("dic" + sonastik.getDifficulty() +".txt");
+        System.out.println(sonastik);
+
+    }
+}
